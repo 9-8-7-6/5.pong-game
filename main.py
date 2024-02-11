1cb79e8e001f91efb4fdeca6d3dba1b55f7e4b43
vito <ac1246897@gmail.com>
2024-02-11 19:51:49 +0800

    updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from turtle import Screen,Turtle
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,30 +9,38 @@ screen.setup(width=800, height=600)
 screen.title("pong")
 screen.tracer(0)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-
-
-def move_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-def move_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+screen.listen()
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
-screen.listen()
-screen.onkey(move_up, "Up")
-screen.onkey(move_down, "Down")
+    #detect collision with th wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.xcor() < -400:
+        ball.reset()
+
+    if ball.xcor() > 400:
+        ball.reset()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 348:
+        ball.bounce_x()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -348:
+        ball.bounce_x()
+
+
 
 screen.exitonclick()
