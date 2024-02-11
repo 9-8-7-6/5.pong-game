440615a95c094346a13e38895dbfe464a000452b
vito <ac1246897@gmail.com>
2024-02-11 20:04:43 +0800

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.move_up, "Up")
@@ -29,11 +31,13 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    if ball.xcor() < -400:
+    if ball.xcor() < -380:
         ball.reset()
+        scoreboard.r_point()
 
-    if ball.xcor() > 400:
+    if ball.xcor() > 380:
         ball.reset()
+        scoreboard.l_point()
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 348:
         ball.bounce_x()
